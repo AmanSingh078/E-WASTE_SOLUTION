47d93bd5ff308f037e92273fb1d89a1e3aa97e31
AmanSingh078 <singhaman58408@gmail.com>
2025-03-15 04:28:00 +0530

    Add files via upload

---
+++ try/js/javaspit.js
@@ -0,0 +1,9 @@
+document.getElementById('pickup-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pickup scheduled successfully!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent successfully!');
+});
